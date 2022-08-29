f223ee558576b5566e5a65c7c212649eb6abca01
Adrian Arco <adrian.arco.gonzalez@gmail.com>
2022-08-30 00:06:47 +0200

    install styled-components and add registerStyled

---
+++ src/Register/RegisterStyled.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const RegisterStyled = styled.section`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: black;
+`;
+
+export default RegisterStyled;
