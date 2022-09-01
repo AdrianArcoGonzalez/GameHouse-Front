9fd4d98a3503573e99d6a5c1685cd7bd2f99b451
Adrian Arco <adrian.arco.gonzalez@gmail.com>
2022-09-01 22:29:32 +0200

    add herosection test

--- src/components/HeroSection/HeroSection.tsx
+++ src/components/HeroSection/HeroSection.tsx
@@ -2,11 +2,11 @@ import HeroSectionStyled from "./HeroSectionStyled";
 
 const HeroSection = (): JSX.Element => {
   return (
-    <HeroSectionStyled>
+    <HeroSectionStyled data-testid="section">
       <div className="hero-container">
         <p className="hero-container__sentence">
           Become a soldier of the GameHouse Army. Make your own reviews and
-          receive feedback from other soldiers.
+          receive feedback from other soldiers!
         </p>
       </div>
     </HeroSectionStyled>
