2a36daa5755cce62c2327bf2ff88d2a1eb0373bd
Adrian Arco <adrian.arco.gonzalez@gmail.com>
2022-09-02 11:52:51 +0200

    add herotext ts and refactor header to get src image from props

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,11 +2,16 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import Header from "../Header/Header";
 import HeroSection from "../HeroSection/HeroSection";
 import RegisterPage from "../../pages/RegisterPage";
+import heroTexts from "../HeroSection/heroData/heroData";
+
 const App = (): JSX.Element => {
   return (
     <>
       <Header />
-      <HeroSection />
+      <HeroSection
+        text={heroTexts.register}
+        srcImage={"images/form-background.png"}
+      />
       <Routes>
         <Route path="/" element={<Navigate to="/register" />} />
         <Route path="/register" element={<RegisterPage />} />
---
+++ src/components/HeroSection/heroData/heroData.ts
@@ -0,0 +1,10 @@
+const heroTexts = {
+  home: "Cuanto más te acerques a la luz, mayor será tu sombra (Kingdom Hearts)",
+  register:
+    "Become a soldier of the GameHouse Army. Make your own reviews and receive feedback from other soldiers.",
+  signIn:
+    "A strong man doesn’t need to read the future. He makes his own.” (Solid Snake)",
+  details:
+    "“We look up at the same starts and see such diferent things.”(Jon Snow)",
+};
+export default heroTexts;
