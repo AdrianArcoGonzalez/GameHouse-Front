3abc1c4946d7cad9514c75b8c46ba4795f46e98b
Adrian Arco <adrian.arco.gonzalez@gmail.com>
2022-09-05 13:39:07 +0200

    add act and wait for on test usegameApi

--- src/hooks/useGamesApi.test.tsx
+++ src/hooks/useGamesApi.test.tsx
@@ -1,4 +1,4 @@
-import { renderHook } from "@testing-library/react";
+import { act, renderHook, waitFor } from "@testing-library/react";
 import axios from "axios";
 import { Wrapper } from "../utils/Wrapper";
 import useGamesApi from "./useGamesApi";
@@ -13,7 +13,6 @@ jest.mock("react-redux", () => ({
 }));
 
 describe("Given a useGamesApi custom hook", () => {
-  beforeEach(() => jest.restoreAllMocks());
   describe("When it's invoked with getAllGames method", () => {
     test("Then it should dispatch all games received", async () => {
       const {
@@ -21,9 +20,14 @@ describe("Given a useGamesApi custom hook", () => {
           current: { getAllGames },
         },
       } = renderHook(useGamesApi, { wrapper: Wrapper });
-      await getAllGames();
 
-      expect(mockDispatch).toHaveBeenCalled();
+      await act(async () => {
+        await getAllGames();
+      });
+
+      await waitFor(() => {
+        expect(mockDispatch).toHaveBeenCalled();
+      });
     });
 
     test("And if it get an error getting all games it should call the error toast", async () => {
