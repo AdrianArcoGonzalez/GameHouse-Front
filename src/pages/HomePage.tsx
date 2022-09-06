25869b93851b69caac5ad9f125743ce7f758243e
Adrian Arco <adrian.arco.gonzalez@gmail.com>
2022-09-06 11:15:27 +0200

    delete toast container from pages and ad it on index

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,4 +1,3 @@
-import { ToastContainer } from "react-toastify";
 import heroTexts from "../components/HeroSection/heroData/heroData";
 import HeroSection from "../components/HeroSection/HeroSection";
 import "react-toastify/dist/ReactToastify.css";
@@ -9,7 +8,6 @@ import JoinUs from "../components/JoinUs/JoinUs";
 const HomePage = (): JSX.Element => {
   return (
     <HomePageStyled>
-      <ToastContainer />
       <HeroSection text={heroTexts.home} srcImage={"images/home.webp"} />
       <h2 className="games-list__title">Comunity Games</h2>
       <Games />
