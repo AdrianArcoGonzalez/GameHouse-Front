6dad8faa77ff21637567badf2c2d7edf5635d6e8
Adrian Arco <adrian.arco.gonzalez@gmail.com>
2022-09-07 22:30:43 +0200

    add test to games component

--- src/components/Games/Games.test.tsx
+++ src/components/Games/Games.test.tsx
@@ -1,18 +1,38 @@
 import { render, screen } from "@testing-library/react";
-import { Wrapper } from "../../utils/Wrapper";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import { mockGameArray } from "../../mocks/mockGame";
+import { store } from "../../store/store";
 import Games from "./Games";
 
+const mockAppSelector = mockGameArray;
+jest.mock("../../store/hooks", () => ({
+  ...jest.requireActual("../../store/hooks"),
+  useAppSelector: () => mockAppSelector,
+}));
 describe("Given a Games component", () => {
   describe("When it's instantiated", () => {
     test("Then it should render a list of games", () => {
-      render(<Games />, { wrapper: Wrapper });
+      render(
+        <BrowserRouter>
+          <Provider store={store}>
+            <Games />
+          </Provider>
+        </BrowserRouter>
+      );
       const list = screen.getByRole("list");
 
       expect(list).toBeInTheDocument();
     });
 
     test("And it should render the list items", () => {
-      render(<Games />, { wrapper: Wrapper });
+      render(
+        <BrowserRouter>
+          <Provider store={store}>
+            <Games />
+          </Provider>
+        </BrowserRouter>
+      );
       const listItem = screen.getAllByRole("listitem");
 
       listItem.forEach((item) => expect(item).toBeInTheDocument());
