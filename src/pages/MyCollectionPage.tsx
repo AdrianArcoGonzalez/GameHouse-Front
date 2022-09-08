d9f815dd901a9b1fbbf1925059972de7160739a8
Adrian Arco <adrian.arco.gonzalez@gmail.com>
2022-09-08 13:49:53 +0200

    add heroSection on mycollection and change img format

--- src/pages/MyCollectionPage.tsx
+++ src/pages/MyCollectionPage.tsx
@@ -1,5 +1,7 @@
 import { useEffect } from "react";
 import GameDetails from "../components/GameDetails/GameDetails";
+import heroTexts from "../components/HeroSection/heroData/heroData";
+import HeroSection from "../components/HeroSection/HeroSection";
 import useGamesApi from "../hooks/useGamesApi";
 import { useAppSelector } from "../store/hooks";
 
@@ -14,13 +16,19 @@ const MyCollectionPage = (): JSX.Element => {
   }, [getByOwner, username]);
 
   return (
-    <ul>
-      {games.map((game) => (
-        <li>
-          <GameDetails game={game} />
-        </li>
-      ))}
-    </ul>
+    <>
+      <HeroSection
+        srcImage="/images/my-collection.webp"
+        text={heroTexts.myCollection}
+      />
+      <ul>
+        {games.map((game) => (
+          <li>
+            <GameDetails game={game} />
+          </li>
+        ))}
+      </ul>
+    </>
   );
 };
 
