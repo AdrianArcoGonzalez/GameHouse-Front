00a164f11e22696437b795f13be39efb75fbfd0a
Adrian Arco Gonzalez <adrian.arco.gonzalez@gmail.com>
2022-09-18 09:55:58 +0200

    create ui slice and add it to store

---
+++ src/store/slice/uiSlice.ts
@@ -0,0 +1,26 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+interface InitialStateUI {
+  isLoading: boolean;
+  
+}
+const initialStateUI: InitialStateUI = {
+  isLoading: false,
+};
+
+export const UISlice = createSlice({
+  name: "users",
+  initialState: initialStateUI,
+  reducers: {
+    isLoading: (state) => ({...state, isLoading: !initialStateUI.isLoading}),
+
+  },
+});
+
+const UIReducer = UISlice.reducer;
+export const {
+isLoading: isLoadingActionCreator
+} = UISlice.actions;
+
+export default UIReducer;
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,9 +1,10 @@
 import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
 import gamesReducer from "./slice/gamesSlice";
+import UIReducer from "./slice/uiSlice";
 import usersReducer from "./slice/usersSlice";
 
 export const store = configureStore({
-  reducer: { user: usersReducer, games: gamesReducer },
+  reducer: { user: usersReducer, games: gamesReducer, ui:UIReducer },
 });
 
 export type AppDispatch = typeof store.dispatch;
